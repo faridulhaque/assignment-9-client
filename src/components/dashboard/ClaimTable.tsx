59d4e72c41da0b425716416da0016a0262c8376d
faridul haque <faridulhaquemrushed@gmail.com>
2024-05-28 12:24:27 +0600

    claims table added

---
+++ src/components/dashboard/ClaimTable.tsx
@@ -0,0 +1,38 @@
+"use client";
+import { useGetClaimsQuery } from "@/services/otherApi/dashboardApi";
+import React from "react";
+
+const ClaimTable = () => {
+  const { data, isLoading } = useGetClaimsQuery("");
+  const claims = data?.data;
+
+
+  return (
+    <div className="overflow-x-auto">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>Item</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Finder</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* row 1 */}
+          {claims?.map((claim: any) => (
+            <tr key={claim?.id}>
+              <th>{claim?.FoundItem?.category?.name}</th>
+              <td>{claim?.FoundItem?.description}</td>
+              <td>{claim?.status}</td>
+              <td>{claim?.FoundItem?.user?.username}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ClaimTable;
